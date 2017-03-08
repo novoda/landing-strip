72cd00f229599b97e759321e458229bcc2ec819c
Ataul Munim <hello@ataulm.com>
2017-03-08 17:44:18 +0000

    Add custom view for TabsContainer

---
+++ core/src/main/java/com/novoda/landingstrip/TabsContainerView.java
@@ -0,0 +1,13 @@
+package com.novoda.landingstrip;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class TabsContainerView extends LinearLayout {
+
+    public TabsContainerView(Context context) {
+        super(context);
+        super.setOrientation(HORIZONTAL);
+    }
+
+}
