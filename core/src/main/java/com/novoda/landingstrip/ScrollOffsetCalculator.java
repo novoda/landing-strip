6af8cbd4ecd522ad2c09a5e2d1fd005bb9443cb4
Adam Brown <adam@novoda.com>
2017-03-22 21:25:29 +0000

    removing has tab guard since the fastforward will no longer trigger this method with -1

--- core/src/main/java/com/novoda/landingstrip/ScrollOffsetCalculator.java
+++ core/src/main/java/com/novoda/landingstrip/ScrollOffsetCalculator.java
@@ -17,18 +17,14 @@ class ScrollOffsetCalculator {
     }
 
     int calculateScrollOffset(int position, float pagerOffset) {
-        if (!hasTabAt(position)) {
-            return 0;
-        } else {
-            View tabForPosition = tabsContainer.getChildAt(position);
+        View tabForPosition = tabsContainer.getChildAt(position);
 
-            float tabStartX = tabForPosition.getLeft() + getHorizontalScrollOffset(tabForPosition, pagerOffset);
-            float viewMiddleOffset = rootView.getWidth() * HALF_MULTIPLIER;
-            float tabCenterOffset = (tabForPosition.getRight() - tabForPosition.getLeft()) * HALF_MULTIPLIER;
-            float nextTabDelta = getNextTabDelta(position, pagerOffset, tabForPosition);
+        float tabStartX = tabForPosition.getLeft() + getHorizontalScrollOffset(tabForPosition, pagerOffset);
+        float viewMiddleOffset = rootView.getWidth() * HALF_MULTIPLIER;
+        float tabCenterOffset = (tabForPosition.getRight() - tabForPosition.getLeft()) * HALF_MULTIPLIER;
+        float nextTabDelta = getNextTabDelta(position, pagerOffset, tabForPosition);
 
-            return roundToInt(tabStartX - viewMiddleOffset + tabCenterOffset + nextTabDelta);
-        }
+        return roundToInt(tabStartX - viewMiddleOffset + tabCenterOffset + nextTabDelta);
     }
 
     private int roundToInt(float input) {
